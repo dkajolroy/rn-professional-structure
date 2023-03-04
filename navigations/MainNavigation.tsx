5be359084182ee746e86227d0ef95f64971dbc54
kajolroy <shrikajol@gmail.com>
2023-03-04 16:13:43 +0600

    route

--- navigations/MainNavigation.tsx
+++ navigations/MainNavigation.tsx
@@ -1,17 +1,29 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {ROUTES} from '../constants/routes';
+import {RootStackParamList} from '../interface/NavInterface';
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 export default function MainNavigation() {
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator<RootStackParamList>();
 
+  const config = {
+    animation: 'spring',
+    config: {
+      stiffness: 1000,
+      damping: 500,
+      mass: 3,
+      overshootClamping: true,
+      restDisplacementThreshold: 0.01,
+      restSpeedThreshold: 0.01,
+    },
+  };
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name={ROUTES.home} component={HomeScreen} />
-        <Stack.Screen name={ROUTES.profile} component={HomeScreen} />
+      <Stack.Navigator screenOptions={{}}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
