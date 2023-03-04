76ab3633a87dd0993bf9d82f3f543cfb5b7368d6
kajolroy <shrikajol@gmail.com>
2023-03-04 14:55:23 +0600

    route config

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
-import HomeScreen from './screens/HomeScreen';
+import MainNavigation from './navigations/MainNavigation';
 import {store} from './store/store';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <MainNavigation />
     </Provider>
   );
 }
--- navigations/MainNavigation.tsx
+++ navigations/MainNavigation.tsx
@@ -1,6 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
+import {ROUTES} from '../constants/routes';
 import HomeScreen from '../screens/HomeScreen';
 
 export default function MainNavigation() {
@@ -9,7 +10,8 @@ export default function MainNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name={ROUTES.home} component={HomeScreen} />
+        <Stack.Screen name={ROUTES.profile} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
