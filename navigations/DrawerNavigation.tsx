915e3a412f1736fda67af843b82ec4ea29bf2741
kajolroy <shrikajol@gmail.com>
2023-03-05 16:45:08 +0600

    add nav compo

---
+++ navigations/DrawerNavigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function DrawerNavigation() {
+  return (
+    <View>
+      <Text>DrawerNavigation</Text>
+    </View>
+  );
+}
